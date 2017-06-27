e87fc2b1ab056f026fe148e89ce03e4f7fa13f1a
David Clark <david.dave.clark@gmail.com>
2017-06-27 11:18:24 -0700

    Trrravis (#1)

--- test/test.js
+++ test/test.js
@@ -6,6 +6,9 @@ const vnuValidateHtml = require('..');
 const fixture = name => path.join(__dirname, 'fixtures', name + '.html');
 
 describe('validate', () => {
+  // Java can be very slow on Travis.
+  global.jasmine.DEFAULT_TIMEOUT_INTERVAL = 40000;
+
   test('basically works', () => {
     return vnuValidateHtml.validate(fixture('one')).then(report => {
       expect(report).toMatchSnapshot();
